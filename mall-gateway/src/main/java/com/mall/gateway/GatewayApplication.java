ad6642c52eefe6aafd8c18d3542a408b3e7faa02
KKKK <1310153668@qq.com>
2025-02-26 15:48:27 +0800

    实现分发jwt-token、校验jwt-token，用户登录、用户id注入全局，可以直接调用common里的UserContext获取用户id

--- mall-gateway/src/main/java/com/mall/gateway/GatewayApplication.java
+++ mall-gateway/src/main/java/com/mall/gateway/GatewayApplication.java
@@ -1,11 +1,15 @@
 package com.mall.gateway;
 
+import com.mall.gateway.config.AuthProperties;
+import com.mall.gateway.config.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 
 //springboot程序的启动类
 @SpringBootApplication
+@EnableConfigurationProperties({AuthProperties.class, JwtProperties.class})
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class);
---
+++ mall-gateway/src/main/java/com/mall/gateway/filter/AuthGlobalFilter.java
@@ -0,0 +1,86 @@
+package com.mall.gateway.filter;
+
+//import com.mall.common.exception.UnauthorizedException;
+import com.mall.gateway.utils.CollUtils;
+import com.mall.gateway.config.AuthProperties;
+import com.mall.gateway.utils.JwtTool;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@EnableConfigurationProperties(AuthProperties.class)
+public class AuthGlobalFilter implements GlobalFilter, Ordered {
+
+    private final JwtTool jwtTool;
+
+    private final AuthProperties authProperties;
+
+    private final AntPathMatcher antPathMatcher = new AntPathMatcher();
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+//        System.out.println("过滤器执行了");
+        // 1.获取Request
+        ServerHttpRequest request = exchange.getRequest();
+        // 2.判断是否不需要拦截
+        if(isExclude(request.getPath().toString())){
+            // 无需拦截，直接放行
+//            System.out.println("不用过滤");
+            return chain.filter(exchange);
+        }
+//        System.out.println("需要过滤");
+        // 3.获取请求头中的token
+        String token = null;
+        List<String> headers = request.getHeaders().get("authorization");
+        if (!CollUtils.isEmpty(headers)) {
+            token = headers.get(0);
+        }
+        // 4.校验并解析token
+        Long userId = null;
+        try {
+            userId = jwtTool.parseToken(token);
+        } catch (RuntimeException e) {
+            // 如果无效，拦截
+//            System.out.println("拦截");
+            ServerHttpResponse response = exchange.getResponse();
+            response.setRawStatusCode(401);
+            return response.setComplete();
+        }
+
+        // TODO 5.如果有效，传递用户信息
+//        System.out.println("userId = " + userId);
+//        System.out.println("token 有效");
+        String userInfo = userId.toString();
+        ServerWebExchange ex = exchange.mutate()
+                .request(b -> b.header("user-info", userInfo))
+                .build();
+        // 6.放行
+        return chain.filter(exchange);
+    }
+
+    private boolean isExclude(String antPath) {
+        for (String pathPattern : authProperties.getExcludePaths()) {
+            if(antPathMatcher.match(pathPattern, antPath)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
