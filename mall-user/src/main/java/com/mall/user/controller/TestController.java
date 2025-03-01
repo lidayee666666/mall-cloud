9d1011f60e894dee2a75b8df032b80c7ac5c75ef
liqinglong <3156620753@qq.com>
2025-03-01 11:28:15 +0800

    test

--- mall-user/src/main/java/com/mall/user/controller/TestController.java
+++ mall-user/src/main/java/com/mall/user/controller/TestController.java
@@ -1,22 +1,19 @@
 package com.mall.user.controller;
 
 import com.mall.common.utils.UserContext;
-import com.mall.user.service.LoginService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @RestController
 public class TestController {
 
     @PostMapping("/users/api/test")
     public void test() {
+        CopyOnWriteArrayList<Object> a1 = new CopyOnWriteArrayList<>();
+        ConcurrentHashMap a2 = new ConcurrentHashMap();
         System.out.println(UserContext.getUser());
     }
 }
