ebe1fb63150407a45eab029509a8f1d696829d5d
liqinglong <3156620753@qq.com>
2025-03-01 13:20:55 +0800

    防止密码序列化

--- mall-user/src/main/java/com/mall/user/pojo/User.java
+++ mall-user/src/main/java/com/mall/user/pojo/User.java
@@ -3,6 +3,7 @@ package com.mall.user.pojo;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +17,8 @@ public class User {
     @TableId(type = IdType.AUTO)
     private Integer id;
     private String username;
+    //序列化时不会将密码序列化,防止密码传至前端
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     private String phone;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Shanghai")
