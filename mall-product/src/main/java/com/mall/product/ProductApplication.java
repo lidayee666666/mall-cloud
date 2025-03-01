98d97cd15ddedbce39dabd71b00e4c4ebad332c5
liqinglong <3156620753@qq.com>
2025-03-01 11:27:16 +0800

    test

--- mall-product/src/main/java/com/mall/product/ProductApplication.java
+++ mall-product/src/main/java/com/mall/product/ProductApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+
 //springboot程序的启动类
 @SpringBootApplication
 //扫描
