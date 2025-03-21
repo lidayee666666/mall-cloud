71e961410d1183ec90d5e8a6c46a66e012883b60
李青龙 <3156620753@qq.com>
2025-03-21 15:05:44 +0800

    李青龙--2025/3/21

--- mall-user/src/main/java/com/mall/user/config/JwtProperties.java
+++ mall-user/src/main/java/com/mall/user/config/JwtProperties.java
@@ -12,6 +12,6 @@ public class JwtProperties {
     private Resource location;
     private String password;
     private String alias;
-    private Duration tokenTTL = Duration.ofMinutes(10);
+    private Duration tokenTTL = Duration.ofDays(10);
 }
 
