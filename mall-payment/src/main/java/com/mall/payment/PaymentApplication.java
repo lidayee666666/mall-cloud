dab53b7cad8e76e7f30c2576aef14618ce0b8c41
李青龙 <3156620753@qq.com>
2025-03-29 17:31:36 +0800

    李青龙--2025/3/29

--- mall-payment/src/main/java/com/mall/payment/PaymentApplication.java
+++ mall-payment/src/main/java/com/mall/payment/PaymentApplication.java
@@ -1,9 +1,15 @@
 package com.mall.payment;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-
+//扫描
+@MapperScan("com.mall.payment.mapper")
+@ComponentScan({
+        "com.mall.common"
+})
 @SpringBootApplication
 public class PaymentApplication {
     public static void main(String[] args) {
--- mall-store/src/main/java/com/mall/store/StoreApplication.java
+++ mall-store/src/main/java/com/mall/store/StoreApplication.java
@@ -1,8 +1,10 @@
 package com.mall.store;
 
+import com.mall.api.config.DefaultFeignConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -11,6 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
         "com.mall.common",
         "com.mall.store"
 })
+@EnableFeignClients(basePackages = "com.mall.api.client", defaultConfiguration = DefaultFeignConfig.class)
 public class StoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(StoreApplication.class);
