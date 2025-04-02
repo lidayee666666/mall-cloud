b49e10ab1525f91b85be020cc72713796b49281d
李青龙 <3156620753@qq.com>
2025-04-02 16:31:52 +0800

    李青龙--2025/4/2

--- mall-common/src/main/java/com/mall/common/config/MvcConfig.java
+++ mall-common/src/main/java/com/mall/common/config/MvcConfig.java
@@ -2,6 +2,7 @@ package com.mall.common.config;
 
 import com.mall.common.interceptor.UserInfoInterceptor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -10,8 +11,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @ConditionalOnClass(DispatcherServlet.class)
 public class MvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public UserInfoInterceptor userInfoInterceptor() {
+        return new UserInfoInterceptor();
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new UserInfoInterceptor());
+        registry.addInterceptor(userInfoInterceptor());
     }
 }
