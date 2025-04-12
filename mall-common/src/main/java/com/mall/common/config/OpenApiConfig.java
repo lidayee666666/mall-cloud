cc2b337df500e01dd157e0c785aec0e8aef09225
李青龙 <3156620753@qq.com>
2025-04-12 18:49:58 +0800

    李青龙--2025/4/12

--- mall-common/src/main/java/com/mall/common/config/OpenApiConfig.java
+++ mall-common/src/main/java/com/mall/common/config/OpenApiConfig.java
@@ -16,8 +16,8 @@ public class OpenApiConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("青龙商城API文档")
+                        .title("StarMarket商城API文档")
                         .version("1.0")
-                        .description("青龙商城微服务系统API文档"));
+                        .description("StarMarket商城微服务系统API文档"));
     }
 }
